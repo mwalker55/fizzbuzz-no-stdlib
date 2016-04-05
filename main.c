01e6d1d8e35779beda5346e022a69df8e72a6708
Mark Walker <mark.walker620@gmail.com>
2016-04-04 18:52:01 -0700

    Added code

---
+++ main.c
@@ -0,0 +1,46 @@
+#define FIB_MAX 100
+#include "unistd.h"
+
+size_t strlen(char *str) {
+	size_t len = 0;
+	while(*(str++)){
+		len+=1;
+	}
+	return len;
+}
+
+void intToText(int num, char* buf){
+	char *ptr = buf;
+	size_t size = 0;
+
+	while(num > 0){
+		*(ptr++) = num%10 + '0';
+		size++;
+		num/=10;
+	}
+
+	for(int a = 0, b = size-1; a < b; a++, b--){
+		char temp = buf[a];
+		buf[a] = buf[b];
+		buf[b] = temp;
+	}
+	buf[size] = '\n';
+	buf[size+1] = 0;
+}
+
+int main(void){
+	char buf[64];
+	int i;
+	for(i = 1; i < FIB_MAX; i++){
+		if(i%3==0&&i%5==0){
+			write(1, "FizzBuzz\n", 9);
+		} else if (i%3==0){
+			write(1, "Fizz\n", 5);
+		} else if (i%5==0){
+			write(1, "Buzz\n", 5);
+		} else {
+			intToText(i, buf);
+			write(1, buf, strlen(buf));
+		}
+	}
+}
